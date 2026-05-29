e195f98c924ad5b26e371a84d951e20e7deb22e7
kuro <no1122057@gmail.com>
2026-05-29 15:50:09 +0500

    dv

--- docs/swagger_ride.go
+++ docs/swagger_ride.go
@@ -12,7 +12,7 @@ package docs
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:80/ride
+// @host      192.168.223.128:80/ride
 // @BasePath  /
 
 // @securityDefinitions.apikey BearerAuth
